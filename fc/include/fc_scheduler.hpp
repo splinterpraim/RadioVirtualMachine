5197b4b65d16588c8633471c7241a36839c376b4
krylelena <krylelena99@yandex.ru>
2023-03-19 09:19:59 +0300

    Created class Sheduler (empty) for future release

---
+++ fc/include/fc_scheduler.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file fc.hpp
+ * @author Elena Potapova (krylelena99@yandex.ru)
+ * @brief Scheduler for front-end compiler
+ * @version 0.2
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef FC_SCHEDULER_CLASS
+#define FC_SCHEDULER_CLASS
+
+class fc_Scheduler
+{
+private:
+};
+
+#endif // FC_SCHEDULER_CLASS
